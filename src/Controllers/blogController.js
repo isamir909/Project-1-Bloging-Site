b447c5d59a41677f0f94f628eac306572be16206
isamir909 <samirlohiya909@gmail.com>
2022-06-21 21:33:54 +0530

    edited author controller

--- src/Controllers/blogController.js
+++ src/Controllers/blogController.js
@@ -1,11 +1,16 @@
+const AuthorModel = require("../Models/AuthorModel")
 const blogModel = require("../Models/BlogModel")
 
 const createBlog = async function(req, res){
 
     try{
         let data = req.body
+        authorid=req.body.authorid
+        AuthorModel.
+        if(authorid)
         let savedData = await blogModel.create(data)
-        res.status(200).send({msg: savedData})
+
+        res.status(201).send({msg: savedData})
         }
         catch(error){
         res.status(500).send({msg: err})
