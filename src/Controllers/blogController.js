33e2e52d643e4fe05b1615892053499e82f331f9
animeshj2132 <ajangir52@gmail.com>
2022-06-21 22:16:23 +0530

    blog api

--- src/Controllers/blogController.js
+++ src/Controllers/blogController.js
@@ -1,20 +1,35 @@
-const AuthorModel = require("../Models/AuthorModel")
+const AuthorModel = require('../Models/AuthorModel');
 const blogModel = require("../Models/BlogModel")
+
 
 const createBlog = async function(req, res){
 
     try{
         let data = req.body
-        authorid=req.body.authorid
-        AuthorModel.
-        if(authorid)
-        let savedData = await blogModel.create(data)
+        let Id = data.AuthorId
+        let authId = await AuthorModel.findById(Id) 
+        
+        if(!authId){
+            res.status(404).send({msg: 'invalid ID'})
+        }      
+       
+        else if (!('title' in data) || !('body' in data) || !('category' in data))
+       { return res.status(422).send({ msg: "required fields missing" })
+    
+    }
+    else if(data.AuthorId.trim() == "" || data.title.trim() == "" || data.body.trim() == "" || data.catagory.trim() == "" ){
+    return res.status(400).send({ msg: "empty field" })}
 
-        res.status(201).send({msg: savedData})
-        }
+    else{ 
+            let savedData = await blogModel.create(data)
+            res.status(201).send({msg: savedData})
+        
+    }
+    }
         catch(error){
-        res.status(500).send({msg: err})
-    }
-    }
+        res.status(500).send({msg: error.message})
+    }  
+}
+
 
     module.exports.createBlog = createBlog
