c3fa11bc5644f886d19423cd9703fd957ddd3e9f
isamir909 <samirlohiya909@gmail.com>
2022-06-21 12:49:19 +0530

    index.js added

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const bodyParser=('body-parser');
+const route=require('./routes/route');
+const app=express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb+srv://samirlohiya909:Lohiya123@samirlohiya.nszppy8.mongodb.net/Project-1?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/',route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
