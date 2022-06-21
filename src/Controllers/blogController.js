559fb5e379a7d05719bfb214c09a0541c2314a45
aditya8318101728 <106534837+aditya8318101728@users.noreply.github.com>
2022-06-21 22:17:04 +0530

    project

--- src/Controllers/blogController.js
+++ src/Controllers/blogController.js
@@ -14,50 +14,7 @@ const createBlog = async function(req, res){
     }
 
 
-    // const createBlogData = async function (req,res){
-
-    // try{
-    //     let data = req.body;
-    //     let Id = data.authorId;
-    //     if(!Id){
-    //         res.status(400).send("enter author id")
-    //         return;
-    //     }
-    //     let authorId = await AuthorModel.findById(Id);
-    //     if(!authorId){
-    //         res.send(400).send("please enter valid Id")
-    //     }
-    //     let result = await BlogModel.create(data)
-    //     res.status(201).send({
-    //         status:true,
-    //         data:result
-    //     })
-    // }catch(err){
-    //     res.status(500).send({
-    //         status:false,
-    //         msg:err.message
-    //     })
-    // }
-    // }
-    const blogUpdate = async function (req,res){
-        try{
-            let id = req.params.BlogModel
-            let findid = await BlogModel.findById (id)
-            let (findId) = res.status(400).send({status:"False",message:"Id not exits"})
-            let data = req.body
-            let dataToUpdate = await BlogModel.findOneAndUpdate({_id:id},data,{new:true})
-                res.status(201).send({status:"True",data:dataToUpdated}) 
-
-        }
-        catch(err){
-            res.status(500).send({
-            status:false,
-            msg:err.message
-            })
-    }
-    }
-
 
     module.exports.createBlog = createBlog
     module.exports.createBlogData = createBlogData
-    module.exports.blogUpdate = blogUpdate
+    
