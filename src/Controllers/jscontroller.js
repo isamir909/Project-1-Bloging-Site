87d2c44dfe989b307b34903996332f10a4dd4a6b
isamir909 <samirlohiya909@gmail.com>
2022-06-21 22:46:03 +0530

    updated delete.js

--- src/Controllers/jscontroller.js
+++ src/Controllers/jscontroller.js
@@ -4,6 +4,7 @@ const authorModel = require("../Models/AuthorModel")
 const getblog = async function (req,res) {
      let query = req.query
      let data = await blogModel.find({$and:[query,{isDeleted:false},{isPublished:true}]})
+     
      if(!data) res.status(404).send({status:"False",msg:"The Data You Found Is not Present"})
      res.status(200).send({msg:"true",data:data})
 }
