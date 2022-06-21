afd0d3e24c91f87eac2a064f8f4734c52bacc8fa
Vaishnavi Sharma <sharmavaishnavi740@gmail.com>
2022-06-21 22:04:36 +0530

    added the blog file

--- src/Controllers/blogController.js
+++ src/Controllers/blogController.js
@@ -1,9 +1,15 @@
+const AuthorModel = require("../Models/AuthorModel")
 const blogModel = require("../Models/BlogModel")
 
 const createBlog = async function(req, res){
 
     try{
         let data = req.body
+        let Id = data.authorId
+        let authorId = await AuthorModel.findById(Id)
+        if(!authId) res.status(400).send({msg:"The AuthorId That You Have written is Invalid"})
+        publishedAt = new Date().to
+
         let savedData = await blogModel.create(data)
         res.status(200).send({msg: savedData})
         }
@@ -12,4 +18,15 @@ const createBlog = async function(req, res){
     }
     }
 
+
+
+
+
+
+
+
+
+
+
+    
     module.exports.createBlog = createBlog
