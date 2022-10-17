750ff6ee71a4790b2fc0b4cb838cb7a2ce39c931
isamir909 <samirlohiya909@gmail.com>
2022-10-17 20:21:17 +0530

    updated user controller

--- src/Controllers/authorController.js
+++ src/Controllers/authorController.js
@@ -1,116 +1,83 @@
 const authorModel = require("../Models/AuthorModel");
 let validator = require("validator");
-let evalidator = require("email-validator");
+let eValidator = require("email-validator");
 let jwt = require("jsonwebtoken");
+const {isValid}=require("../validator/validations")
 
 
 const createAuthor = async function (req, res) {
   try {
     let data = req.body;
     let { email, fname, title, lname, password } = data;
-    let array1 = [email, fname, title, lname, password];
-    let arrayOfString = ["email", "fname", "title", "lname", "password"];
-   
 
-    //if input is empty
+    //if input is empty || required fields are missing
     if (Object.keys(data).length == 0)
-      return res
-        .status(400)
-        .send({ status: false, msg: " input object can not be empty" });
+      return res.status(400).send({ status: false, msg: " input object can not be empty" });
 
-    for (let i = 0; i < array1.length; i++) {
-      // required field verification
-      let value = arrayOfString[i];
-      if (!(value in data))
-        return res.status(422).send({ status: false, msg: value + " " + "is required field" });
-      // status code  422 (Unprocessable Entity)
-
-      //data type verification
-      let type = typeof array1[i];
-      if (type == "object")
-        return res
-          .status(400)
-          .send({ status: false, msg: "input data can not be null" });
-      if (type != "string")
-        return res
-          .status(400)
-          .send({ status: false, msg: "input data must be string" });
-      if (array1[i].toLowerCase() == "undefined")
-        return res
-          .status(400)
-          .send({ status: false, msg: "input data can not be undefined" });
-
-      // condition for empty fields
-      if (array1[i].trim() == "")
-        return res
-          .status(400)
-          .send({ msg: arrayOfString[i] + "" + " can not be empty" });
+    if(!isValid(email)){
+      return res.status(400).send({ status: false, msg: " email is required" });
+    }
+    if(!isValid(fname)){
+      return res.status(400).send({ status: false, msg: " fname is required" });
+    }
+    if(!isValid(lname)){
+      return res.status(400).send({ status: false, msg: " lname is required" });
+    }
+    if(!isValid(title)){
+      return res.status(400).send({ status: false, msg: " title is required" });
+    }
+    if(!isValid(password)){
+      return res.status(400).send({ status: false, msg: " password is required" });
     }
 
     // email validation
-    let validate = evalidator.validate(data.email);
-    if (validate == false)
-      return res
-        .status(400)
-        .send({status: false,msg: "You have entered an invalid email address!"});
+    if (!eValidator.validate(data.email))return res.status(400).send({status: false,msg: "You have entered an invalid email address!"});
 
     // name alphabatic  validation 
-
     let LnameValidate = validator.isAlpha(data.lname);
     let FnameValidate = validator.isAlpha(data.fname);
 
     if (LnameValidate == false || FnameValidate == false)
-      return res
-        .status(400).send({status: false,msg: "LastName and firstName must be between A-z or a-z ",});
+      return res.status(400).send({status: false,msg: "LastName and firstName must be between A-z or a-z ",});
 
-    if (!["Mr", "Mrs", "Miss"].includes(data.title.trim()))
-      return res
-        .status(400)
-        .send({ status: false, msg: "title must be  Mr,Mrs or Miss" });
+      // used map to reduce time complexity
+      let map={"Mr":1, "Mrs":1, "Miss":1}
+      if(map[title.trim()]===undefined){
+        return res.status(400).send({ status: false, msg: "title must be  Mr,Mrs or Miss" });
+      }
+    // if (!["Mr", "Mrs", "Miss"].includes(data.title.trim()))
+    //   return res.status(400).send({ status: false, msg: "title must be  Mr,Mrs or Miss" });
 
     let checkAuthor = await authorModel.findOne({ email: email });
-    if (checkAuthor)
-      return res
-        .status(400)
-        .send({ status: false, msg: "this email is already in use" });
+    if (checkAuthor)return res.status(400).send({ status: false, msg: "this email is already in use" });
+   
     let savedData = await authorModel.create(data);
     res.status(201).send({ status: true, data: savedData });
+
   } catch (error) {
-    console.log(error);
     res.status(500).send({ msg: error.message });
   }
 };
 
 
-
-let loginauth = async function (req, res) {
+let loginAuthor = async function (req, res) {
   try {
     let data = req.body;
     let { email, password } = data;
 
-    //if input is null
-    if (typeof password == "object" || typeof email == "object")
-      return res
-        .status(400)
-        .send({ status: false, msg: "input data can not be null" });
-
-    // if input field is empty
-    if (email.trim() == "" || password.trim() == "")
-      return res
-        .status(400)
-        .send({ status: false, msg: "email and password can not be empty" });
+   if(!isValid(email)){
+    return res.status(400).send({ status: false, msg: " email is required" });
+   }
+   if(!isValid(password)){
+    return res.status(400).send({ status: false, msg: " password is required" });
+  }
     // email validation
-    let validate = evalidator.validate(email);
+    let validate = eValidator.validate(email.trim());
     if (validate == false)
-      return res
-        .status(400)
-        .send({
-          status: false,
-          msg: "You have entered an invalid email address!",
-        });
+      return res.status(400).send({status: false,msg: "You have entered an invalid email address!"});
 
     // if user not found
-    let validateEmail = await authorModel.findOne({ email: email });
+    let validateEmail = await authorModel.findOne({ email: email.trim() });
     if (!validateEmail)
       return res.status(404).send({ status: false, msg: "user not found" });
 
@@ -118,7 +85,6 @@ let loginauth = async function (req, res) {
 
     if (validateEmail.password != password)
       return res.status(401).send({ status: false, msg: "invalid password" });
-   
 
     let key = jwt.sign(
       {
@@ -134,5 +100,7 @@ let loginauth = async function (req, res) {
   }
 };
 
-module.exports.loginauth = loginauth;
-module.exports.createAuthor = createAuthor;
+module.exports = {loginAuthor,createAuthor}
+
+
+
